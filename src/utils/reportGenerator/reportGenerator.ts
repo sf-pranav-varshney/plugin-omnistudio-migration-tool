914bcf7d6d3acae6d1becc870382dd6bb6260c69
pranav.varshney <pranav.varshney@salesforce.com>
2025-05-22 16:21:38 +0530

    fix: eslint issues

--- src/utils/reportGenerator/reportGenerator.ts
+++ src/utils/reportGenerator/reportGenerator.ts
@@ -32,8 +32,8 @@ export function generateHtmlTable<T>(
             .join('')}
         </tr>
       `
-    )
-    .join('')}
+        )
+        .join('')}
     </thead>
   `;
 
@@ -91,10 +91,14 @@ export function generateHtmlTable<T>(
               />
               ${option}
             </label>
-          `).join('')}
+          `
+            )
+            .join('')}
         </div>
       </div>
-    `).join('')}
+    `
+      )
+      .join('')}
   </div>
 </div>`;
 
@@ -104,17 +108,24 @@ export function generateHtmlTable<T>(
         .map(
           (row) => `
         <tr>
+        
           ${columns
+            /* eslint-disable @typescript-eslint/no-unsafe-assignment */
+            /* eslint-disable @typescript-eslint/no-unsafe-call */
+            /* eslint-disable @typescript-eslint/no-unsafe-member-access */
             .map((col) => {
-              const key: string= col.key;
-              const title: string= col.title ? col.title(row) : '';
-              const value: string= col.filterValue(row).toString();
+              const key: string = col.key;
+              const title: string = col.title ? col.title(row) : '';
+              const value: string = col.filterValue(row).toString();
               const cellContent: string = col.cell(row);
               const dataAttr: string = ['name', 'oldName'].includes(key) ? `data-name="${value.toLowerCase()}"` : '';
               return `<td ${dataAttr} title="${title}" key="${key}" value="${value}">${cellContent}</td>`;
-            }).join('')}
+            })
+            .join('')}
         </tr>
-      `).join('')}
+      `
+        )
+        .join('')}
         <tr id="no-rows-message" style="display: none;">
           <td colspan="100%" style="text-align: center; padding: 16px; color: #666;">
             No matching records found.
@@ -133,7 +144,8 @@ export function generateHtmlTable<T>(
           <div class="label-value">${header.value}</div>
         </div>
       `
-        ).join('')}
+        )
+        .join('')}
     </div>
   `;
 
